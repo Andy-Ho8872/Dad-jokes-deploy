e6d2c8ef43e621f9bee14365462664aaf2908f30
ANDY <h880702520@gmail.com>
2020-10-20 21:42:32 +0800

    f26303e

---
+++ _nuxt/365aef1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{386:function(t,e,n){"use strict";n.r(e);var r={},l=n(78),component=Object(l.a)(r,(function(){var t=this.$createElement;return(this._self._c||t)("div",[this._v(this._s(this.$route.params.id))])}),[],!1,null,null,null);e.default=component.exports}}]);
