2aed4ce38b0f00496d635b4ec83e264ae5ba8f75
ANDY <h880702520@gmail.com>
2020-10-20 20:28:47 +0800

    first

---
+++ pages/about.vue
@@ -0,0 +1,54 @@
+<template>
+<div>
+  <div class="text-center purple--text">
+    <h1>Hello This is dad jokes</h1>
+    <h2> We are providing some Dad Joke to throw you a wet blaket </h2>
+    <v-divider class="my-4"></v-divider>
+  </div>
+
+  <div>
+    <h1 class="red--text">How to Use it?</h1>
+    <v-card>
+      <v-list color="red lighten-4">
+          <v-list-item v-for="(item, index) in items" :key="index">
+            <v-list-item-content class="display-1">
+              {{ item.text }}
+            </v-list-item-content>
+          </v-list-item>
+      </v-list>
+    </v-card>
+  </div>
+</div>
+  
+</template>
+
+<script>
+export default {
+  // The head() fucntion is to set mata tag which makes a great impact on SEO
+  head() {
+    return {
+      title: 'About Dad Jokes House', // title tag
+      meta: [
+        {
+          hid: 'description',
+          name: 'description', /// description tag
+          content: 'A nice place to hear some dad jokes '
+        }
+      ]
+    }
+  },
+  data() {
+    return {
+      items: [
+        { text: '1.Go to the "Jokes" page by clicking Navbar button' },
+        { text: '2.Click "ADD A JOKE" to get a radom dad joke' },
+        { text: '3.Click "SEE JOKES" to get all dad jokes that you just added' }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <div class="text-center">
+        <h1 class="my-6">Welcome My Friend</h1>
+        <v-img :src="require('~/assets/img/dadjokes.jpg')"></v-img>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+
+
+export default {
+  head() {
+    return {
+      title: 'Welcome to Dad Jokes House', // title tag
+      meta: [
+        {
+          hid: 'description',
+          name: 'description', /// description tag
+          content: 'The Home page of D J H'
+        }
+      ]
+    }
+  },
+  
+}
+</script>
